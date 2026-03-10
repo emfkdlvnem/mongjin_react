5b4256550d1453130165dafd5eaa055483de05aa
YoonHyeJin <emfkdlvnem@gmail.com>
2026-03-10 10:28:17 +0900

    ㅇㅇ

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import img0 from './assets/image1.jpg';
+import img1 from './assets/image2.jpg';
+import img2 from './assets/image3.jpg';
+let data = [
+  {
+    id: 0,
+    title: 'White and Black',
+    content: 'Born in France',
+    price: 120000,
+    image: img0,
+  },
+
+  {
+    id: 1,
+    title: 'Red Knit',
+    content: 'Born in Seoul',
+    price: 110000,
+    image: img1,
+  },
+
+  {
+    id: 2,
+    title: 'Grey Yordan',
+    content: 'Born in the States',
+    price: 130000,
+    image: img2,
+  },
+];
+
+export default data;
